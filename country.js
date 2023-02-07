387d343ca3d567151280881beea12444bd3b5726
Verbeeren, Eric <EricVerbeeren@RocketCentral.com>
2023-02-06 19:23:15 -0500

    More updates and add switch

--- country.js
+++ country.js
@@ -13,77 +13,69 @@ let belgium = new Country("Belgium", "Flemish", "Hallo", ["black", "yellow", "re
 let germany = new Country("Germany", "German", "Hallo!", ["black", "red", "yellow"])
 
 
-function DisplayColors(Country)
-{
+function DisplayColors(Country) {
     document.getElementById('Color1').style.backgroundColor = Country.colors[0]
-    document.getElementById('Color2').style.backgroundColor = Country.colors[1]
-    document.getElementById('Color3').style.backgroundColor = Country.colors[2]
+    document.getElementById('Color3').style.backgroundColor = Country.colors[1]
+    document.getElementById('Color2').style.backgroundColor = Country.colors[2]
 }
 
 
-function SwitchCountry(name) 
-{
+function SwitchCountry(countrySelected) {
 
-    let input = name;
+    let input = countrySelected;
     document.getElementById('CountryName').style.textAlign = "center";
     document.getElementById('OfficialLanguage').style.textAlign = "center";
     document.getElementById('HelloWorld').style.textAlign = "center";
-    
-    if (input === "USA") 
-    {
-        Country = usa;
-        document.getElementById('CountryName').innerText = Country.name
-        document.getElementById('OfficialLanguage').innerText = Country.lang
-        document.getElementById('HelloWorld').innerText = Country.greeting
-        document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/us.png")
-        DisplayColors(Country)
-    }
-    else if (input === "Mexico") 
-    {
-        Country = mexico;
-        document.getElementById('CountryName').innerText = Country.name
-        document.getElementById('OfficialLanguage').innerText = Country.lang
-        document.getElementById('HelloWorld').innerText = Country.greeting
-        document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/mx.png")
-        DisplayColors(Country)
-    }
-    else if (input === "France")
-     {
-        Country = france;
-        document.getElementById('CountryName').innerText = Country.name
-        document.getElementById('OfficialLanguage').innerText = Country.lang
-        document.getElementById('HelloWorld').innerText = Country.greeting
-        document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/fr.png")
-        DisplayColors(Country)    
-    }
-    else if (input === "Belgium") 
-    {
-        Country = belgium;
-        document.getElementById('CountryName').innerText = Country.name
-        document.getElementById('OfficialLanguage').innerText = Country.lang
-        document.getElementById('HelloWorld').innerText = Country.greeting
-        document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/be.png")
-        DisplayColors(Country)     
-    }
-    else if (input === "Germany") 
-    {
-        Country = germany;
-        document.getElementById('CountryName').innerText = Country.name
-        document.getElementById('OfficialLanguage').innerText = Country.lang
-        document.getElementById('HelloWorld').innerText = Country.greeting
-        document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/de.png")
-        DisplayColors(Country)
-        
-    }
-    else
-    {
-        document.getElementById('CountryName').innerText = ''
-        document.getElementById('OfficialLanguage').innerText = ''
-        document.getElementById('HelloWorld').innerText = ''
-        document.getElementById('Color1').style.backgroundColor = 'gray'
-        document.getElementById('Color2').style.backgroundColor = 'white'
-        document.getElementById('Color3').style.backgroundColor = 'lightgray'
-        document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/un.png")
-        
+
+    switch (input) {
+        case 'USA':
+            Country = usa;
+            document.getElementById('CountryName').innerText = Country.name
+            document.getElementById('OfficialLanguage').innerText = Country.lang
+            document.getElementById('HelloWorld').innerText = Country.greeting
+            document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/us.png")
+            DisplayColors(Country)
+            break;
+        case 'Mexico':
+            Country = mexico;
+            document.getElementById('CountryName').innerText = Country.name
+            document.getElementById('OfficialLanguage').innerText = Country.lang
+            document.getElementById('HelloWorld').innerText = Country.greeting
+            document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/mx.png")
+            DisplayColors(Country)
+            break;
+        case 'France':
+            Country = france;
+            document.getElementById('CountryName').innerText = Country.name
+            document.getElementById('OfficialLanguage').innerText = Country.lang
+            document.getElementById('HelloWorld').innerText = Country.greeting
+            document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/fr.png")
+            DisplayColors(Country)
+            break;
+        case 'Belgium':
+            Country = belgium;
+            document.getElementById('CountryName').innerText = Country.name
+            document.getElementById('OfficialLanguage').innerText = Country.lang
+            document.getElementById('HelloWorld').innerText = Country.greeting
+            document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/be.png")
+            DisplayColors(Country)
+            break;
+        case 'Germany':
+            Country = germany;
+            document.getElementById('CountryName').innerText = Country.name
+            document.getElementById('OfficialLanguage').innerText = Country.lang
+            document.getElementById('HelloWorld').innerText = Country.greeting
+            document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/de.png")
+            DisplayColors(Country)
+            break;
+        default:
+            document.getElementById('CountryName').innerText = 'Hello'
+            document.getElementById('OfficialLanguage').innerText = 'and welcome too'
+            document.getElementById('HelloWorld').innerText = 'countries around the world'
+            document.getElementById('Color1').style.backgroundColor = 'gray'
+            document.getElementById('Color2').style.backgroundColor = 'white'
+            document.getElementById('Color3').style.backgroundColor = 'lightgray'
+            document.getElementById('flagImage').setAttribute('src', "https://flagcdn.com/128x96/un.png")
+            break;
     }
 }
